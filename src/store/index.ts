20b904cb8783642a9cdec54226e84b4d8fbb60fd
Fabiano Alves <fabianoxtx@gmail.com>
2022-06-15 00:06:20 -0300

    Redux Dev Tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,8 @@ export interface IState {
     cart: ICartState;
 }
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    );
 
 export default store;
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -6,17 +6,26 @@ const INITIAL_STATE: ICartState = {
     items: []
 };
 
+
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
     return produce(state, draft => {
         switch(action.type) {
             case 'ADD_PRODUCT_TO_CART': {
                 const { product } = action.payload;
             
-                    draft.items.push({
-                        product,
-                        quantity: 1,
-                    });
-                
+                const productInCartIndex = draft.items.findIndex(item =>
+                    item.product.id ===product.id,
+                    );
+
+                    if ( productInCartIndex >= 0) {
+                        draft.items[productInCartIndex].quantity++;
+                    } else {
+                        draft.items.push({
+                            product,
+                            quantity: 1,
+                        });
+                    }
+                    
                 break;
             }
 
