7b18c850d011bf60671212509df1e71e77278950
Fabiano Alves <fabianoxtx@gmail.com>
2022-06-14 23:29:57 -0300

    Action e States

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/index.ts';
 import Catalog from './components/Catalog.tsx';
+import Cart from './components/Cart.tsx';
 
 export default function App() {
   return (
    <Provider store={store}>
-     <Catalog></Catalog>
+     <Catalog />
+     <Cart />
    </Provider>
   );
 }
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api.ts';
+import { useDispatch } from 'react-redux';
+import { addProductToCart } from '../store/modules/cart/action.ts';
 
 const Catalog: React.FC = () => {
-    const catalog = useSelector(state => state);
-    console.log(catalog)
+        const dispatch = useDispatch();
+        const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+        const handleAddProductToCart = useCallback((product: IProduct) => {
+            dispatch(addProductToCart(product))
+        }, [dispatch])
+
+        useEffect(() => {
+            api.get('/products').then(response => {
+                setCatalog(response.data);
+            })
+        }, []);
     return (
-        <h1>Catalog</h1>
+        <main>
+            <h1>Catalog</h1>
+            {catalog.map(product => (
+                <article key={product.id}>
+                    <strong>{product.title}</strong> { " - "}
+                    <span>{product.price}</span> { "  "}
+
+                    <button 
+                    type="button"
+                    onClick={() => handleAddProductToCart(product)}
+                    >Adicionar</button>
+                </article>
+            ) )}
+        </main>
+        
     );
 
 
