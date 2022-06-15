2f1e30e7852a0c490383ac9b4efb8a8ab11a1e5c
Fabiano Alves <fabianoxtx@gmail.com>
2022-06-15 00:12:21 -0300

    Redux Dev Tools Usage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer.ts'
+//import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension';
 
 export interface IState {
     cart: ICartState;
@@ -8,6 +9,7 @@ export interface IState {
 
 const store = createStore(
     rootReducer,
+    //composeWithDevTools()
     );
 
 export default store;
